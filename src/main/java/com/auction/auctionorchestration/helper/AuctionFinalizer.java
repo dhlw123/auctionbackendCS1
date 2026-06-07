b8ceb15751e00563c0cc3b6ef30022a5d55a3ecb
dhlw123 <dhlwztak@gmail.com>
2026-06-07 12:19:35 -0400

    auto bid bug again

--- src/main/java/com/auction/auctionorchestration/helper/AuctionFinalizer.java
+++ src/main/java/com/auction/auctionorchestration/helper/AuctionFinalizer.java
@@ -1,5 +1,7 @@
 package com.auction.auctionorchestration.helper;
 
+import com.auction.bids.AutoBid;
+import com.auction.bids.BidService;
 import com.auction.items.ItemService;
 import com.auction.itemstatus.ItemStatus;
 import com.auction.itemstatus.ItemStatusRepository;
@@ -8,6 +10,7 @@ import com.auction.users.User;
 import com.auction.users.UserService;
 import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -23,16 +26,19 @@ public class AuctionFinalizer {
   private final ItemStatusService itemStatusService;
   private final ItemService itemService;
   private final UserService userService;
+  private final BidService bidService;
 
   public AuctionFinalizer(
       ItemStatusRepository itemStatusRepository,
       ItemStatusService itemStatusService,
       ItemService itemService,
-      UserService userService) {
+      UserService userService,
+      BidService bidService) {
     this.itemStatusRepository = itemStatusRepository;
     this.itemStatusService = itemStatusService;
     this.itemService = itemService;
     this.userService = userService;
+    this.bidService = bidService;
   }
 
   @Scheduled(fixedDelayString = "${auction.finalizer.delay:30000}")
@@ -51,6 +57,16 @@ public class AuctionFinalizer {
 
         User seller = itemService.getItem(itemId).getUser();
         userService.addBalance(seller.getUsername(), locked.getCurrentPrice());
+
+        // checks if user is auto bidding or not
+        Optional<AutoBid> autobidOP = bidService.getAutoBidByItemId(itemId);
+        if (autobidOP.isPresent()
+            && autobidOP.get().getBidder().getUsername().equals(locked.getHighestBidUser())) {
+          User autoBidder = autobidOP.get().getBidder();
+          userService.addBalance(
+              autoBidder.getUsername(),
+              autobidOP.get().getMaxBidLimit() - locked.getCurrentPrice());
+        }
 
         log.info(
             "Finalized auction for item {}: paid {} to {}",
